7ae733315055d188d00ced5bfbfc309ed3b80834
Olabode Joseph Oluwaseun <josepholabode17@gmail.com>
2022-08-24 02:34:58 +0100

    implemented the insertion into mongodb from coin base api

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -28,7 +28,18 @@ class MongoBackend {
     return false;
   }
 
-  async insert() {}
+  async insert() {
+    const data = await this.coinAPI.fetch();
+    const documents = [];
+    Object.entries(data.bpi).forEach(entry => {
+      documents.push({
+        date: entry[0],
+        value: entry[1]
+      })
+    });
+
+    return this.collection.insertMany(documents);
+  }
 
   async getMax() {}
 
@@ -42,6 +53,12 @@ class MongoBackend {
       throw new Error('Connecting to MongoDB failed');
     }
     console.timeEnd('mongodb-connect');
+
+    console.info('Inserting into MongoDB');
+    console.time('mongodb-insert');
+    const insertResult = await this.insert();
+    console.timeEnd('mongodb-insert');
+    console.info(`Inserted ${insertResult.insertedCount} documents into MongoDB`);
 
     console.info('Disconnecting from MongoDB');
     console.time('mongodb-disconnect');
